1abf445f559cc0816d1b26eb70209c3bca2d3131
s-m-kasianov <s-m-kasianov@users.noreply.github.com>
2022-06-13 15:02:19 +0000

    Build last chages

--- peer/esm/vis-timeline-graph2d.js
+++ peer/esm/vis-timeline-graph2d.js
@@ -5,7 +5,7 @@
  * Create a fully customizable, interactive timeline with items and ranges.
  *
  * @version 0.0.0-no-version
- * @date    2022-06-12T19:28:09.067Z
+ * @date    2022-06-13T15:00:40.779Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2017-2019 visjs contributors, https://github.com/visjs
